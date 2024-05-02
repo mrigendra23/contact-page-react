22dcbde4b176002af191892aac6a7707ae1d2f22
mrigendra <mrigendrasingh845@gmail.com>
2024-05-02 16:30:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+// import Buttons from './components/Button/Button';
+import ContactHeader from './components/contactHeader/ContactHeader';
+import Navigation from './components/navigation/Navigation';
+import ContactForm from './components/ContactForm/ContactForm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navigation/>
+    <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
     </div>
   );
 }
